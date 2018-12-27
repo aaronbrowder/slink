211c16d6bd0bbabb084f98ef65586d9ea6db4e5e
Aaron Browder <aaronbrowder@gmail.com>
2018-12-27 00:36:05 +0000

    Change js baseUrl

--- sites/default/files/js/js_-luQzpUiPJR8JgKYpR8XGNRbWU_GPZnEx98GJ2ulwnM.js
+++ sites/default/files/js/js_-luQzpUiPJR8JgKYpR8XGNRbWU_GPZnEx98GJ2ulwnM.js
@@ -1,3 +1,38 @@
+/* global navigator */
+
+document.addEventListener('DOMContentLoaded', responsiveMenu, false);
+
+function responsiveMenu() {
+    // Responsive details (i.e. the Site Navigation) are closed by default but
+    // should be opened on page load when the screen size is large.
+    var responsiveDetails = document.getElementsByClassName('js-responsive-details');
+    if (screen.width >= 851) {
+        for (var i = 0; i < responsiveDetails.length; i++) {
+            responsiveDetails[i].setAttribute('open', true);   
+        }
+    }
+    // IE does not support HTML5 details, so we need all details elements to
+    // be opened on page load on IE.
+    var isIE = false;
+    if (/MSIE 10/i.test(navigator.userAgent)) {
+       // This is internet explorer 10
+       isIE = true;
+    }
+    if (/MSIE 9/i.test(navigator.userAgent) || /rv:11.0/i.test(navigator.userAgent)) {
+        // This is internet explorer 9 or 11
+        isIE = true;
+    }
+    if (/Edge\/\d./i.test(navigator.userAgent)){
+       // This is Microsoft Edge
+       isIE = true;
+    }
+    if (isIE) {
+        var allDetails = document.getElementsByTagName('details');
+        for (var i = 0; i < allDetails.length; i++) {
+            allDetails[i].setAttribute('open', true);   
+        }   
+    }
+};
 /* global jQuery */
 /* global Drupal */
 
@@ -63,4 +98,4 @@ $(document).ready(function() {
   $('#application-box-canceled').click(function() {
    updateApplication($(this).data('node-id'), false, true); 
   });
-});
+});;
