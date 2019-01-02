2283969ba8519af40dc7b096ecf766153fb3e5fc
Aaron Browder <aaronbrowder@gmail.com>
2019-01-02 00:22:54 +0000

    Don't require subject when commenting

---
+++ sites/default/files/php/twig/5c2c03b80aa41_comment.html.twig_rnOkFXvIgW25oE_G5fUGjSS-U/82SS4xRSeaENL4YAcJbcYNkeGKEeMz10aJQxJ1z0Zho.php
@@ -0,0 +1,122 @@
+<?php
+
+/* themes/slink/templates/comment.html.twig */
+class __TwigTemplate_6c4f727fb36f5f4ef529ac97dacfa333dabf793d1145eadafcb110d965a03e2d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("set" => 68, "if" => 94);
+        $filters = array("without" => 77);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('set', 'if'),
+                array('without'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 68
+        $context["classes"] = array(0 => "comment", 1 => "js-comment", 2 => (((        // line 71
+($context["status"] ?? null) != "published")) ? (("comment--" . ($context["status"] ?? null))) : ("")), 3 => (($this->getAttribute($this->getAttribute(        // line 72
+($context["comment"] ?? null), "owner", array()), "anonymous", array())) ? ("by-anonymous") : ("")), 4 => (((        // line 73
+($context["author_id"] ?? null) && (($context["author_id"] ?? null) == $this->getAttribute(($context["commented_entity"] ?? null), "getOwnerId", array(), "method")))) ? ((("by-" . $this->getAttribute(($context["commented_entity"] ?? null), "getEntityTypeId", array(), "method")) . "-author")) : ("")), 5 => "clearfix");
+        // line 77
+        echo "<article role=\"article\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, twig_without($this->getAttribute(($context["attributes"] ?? null), "addClass", array(0 => ($context["classes"] ?? null)), "method"), "role"), "html", null, true));
+        echo ">
+  ";
+        // line 83
+        echo "  <span class=\"hidden\" data-comment-timestamp=\"";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["new_indicator_timestamp"] ?? null), "html", null, true));
+        echo "\"></span>
+
+  <footer class=\"comment__meta\">
+    ";
+        // line 86
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["user_picture"] ?? null), "html", null, true));
+        echo "
+    <p class=\"comment__author\">";
+        // line 87
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["author"] ?? null), "html", null, true));
+        echo "</p>
+    <p class=\"comment__time\">";
+        // line 88
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["created"] ?? null), "html", null, true));
+        echo "</p>
+    ";
+        // line 94
+        echo "    ";
+        if (($context["parent"] ?? null)) {
+            // line 95
+            echo "      <p class=\"visually-hidden\">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["parent"] ?? null), "html", null, true));
+            echo "</p>
+    ";
+        }
+        // line 97
+        echo "  </footer>
+  <div";
+        // line 98
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute(($context["content_attributes"] ?? null), "addClass", array(0 => "comment__content"), "method"), "html", null, true));
+        echo ">
+    ";
+        // line 99
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["content"] ?? null), "html", null, true));
+        echo "
+  </div>
+</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/slink/templates/comment.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  88 => 99,  84 => 98,  81 => 97,  75 => 95,  72 => 94,  68 => 88,  64 => 87,  60 => 86,  53 => 83,  48 => 77,  46 => 73,  45 => 72,  44 => 71,  43 => 68,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/slink/templates/comment.html.twig", "/home/ubuntu/workspace/themes/slink/templates/comment.html.twig");
+    }
+}
